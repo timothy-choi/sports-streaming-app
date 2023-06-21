7b61f1ebdde339b40beced2c536cdd2b5e80b93b
timothy-choi <tchoi720@gmail.com>
2023-06-20 19:37:55 -0700

    started video service impl

--- server/API/Video/VideoAccountsServiceImpl.java
+++ server/API/Video/VideoAccountsServiceImpl.java
@@ -4,30 +4,40 @@ import API.Video.Video;
 import API.Video.VideoAccounts;
 
 public class VideoAccountsServiceImpl implements VideoAccountsService {
+    @autowired
+    private VideoAccountsRepository videoAccountsRepository;
+
+    @Override
     public List<VideoAccounts> findAll() {
-
+        return videoAccountsRepository.findAll();
     }
 
+    @Override
     public VideoAccounts findByVideoAccountId(String videoAccountId) {
-
+        return videoAccountsRepository.findByVideoAccountId(videoAccountId);
     }
 
+    @Override
     public void deleteVideoAccount(Long videoAccountId) {
 
     }
 
+    @Override
     public void addVideo(Video newVideo) {
 
     }
 
+    @Override    
     public Video getVideo(String username, String videoId) {
 
     }
 
+    @Override
     public void deleteVideo(String username, String videoId) {
 
     }
 
+    @Override
     public List<Video> getVideos(String username) {
 
     }
