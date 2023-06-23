79c843dd27fcecdc6938cc2d66f9e7aed2f1c85a
timothy-choi <tchoi720@gmail.com>
2023-06-23 11:55:08 -0700

    creating schema for communities and embedded docs

--- server/API/Community/Communities.java
+++ server/API/Community/Communities.java
@@ -8,12 +8,84 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import Java.util.UUID;
 
+@Embeddable
 public class User {
+    @Id
+    private @GeneratedValue Long userId;
+    private String name;
+    private String username;
 
+    List<Long> videoPostIds;
+
+    User(String name, String username) {
+        this.userId = UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
+        this.name = name;
+        this.username = username;
+    }
+
+    public Long getUserId() {
+        return this.userId;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public List<Long> getVideoPostIds() {
+        return this.videoPostIds;
+    }
 }
 
+@Embeddable
 public class VideoPost {
-    
+    @Id 
+    private @GeneratedValue Long videoPostId;
+    private String name;
+    private String description;
+    private String Bucket;
+    private String Key;
+    private Int Rating;
+
+    VideoPost(String name, String description, String bucket, String key) {
+        this.videoPostId = UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
+        this.name = name;
+        this.description = description;
+        this.Bucket = bucket;
+        this.Key = key;
+        this.Rating = 1;
+    }
+
+    public Long getVideoPostId() {
+        return this.videoPostId;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getBucket() {
+        return this.Bucket;
+    }
+
+    public String getKey() {
+        return this.Key;
+    }
+
+    public Int getRating() {
+        return this.Rating;
+    }
+
+    public void changeRating(Int rating) {
+        this.Rating = rating;
+    }
 }
 
 
@@ -27,7 +99,10 @@ public class Communities {
     private Int ratingRequirement;
     private Int daysUntilExpiration;
 
+    @Embedded
     List<User> users;
+
+    @Embedded
     List<VideoPost> videoPosts;
 
     Communities(String name, String description, String owner, Int ratingRequirement, Int daysUntilExpiration) {
