d9c49d816f3cb2c9d73bbbc29d27867d5c2921bc
timothy-choi <tchoi720@gmail.com>
2023-07-09 22:05:43 -0700

    added search endpoint

--- server/Search/searchAPI.js
+++ server/Search/searchAPI.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const {createIndex, addDocument, deleteDocument, deleteIndex} = require('./init.js');
+const {createIndex, addDocument, deleteDocument, deleteIndex, searchOperation, getCommunities, getUsers} = require('./init.js');
 
 const PORT = 1100;
 app.listen(PORT, () => {});
@@ -54,3 +54,52 @@ const deleteCommunity = function(req, res) {
 };
 
 app.delete('/community', deleteCommunity);
+
+
+
+const search = function(req, res) {
+    var results = {};
+    try {
+        var searchResults = searchOperation(req.params.searchTerm);
+        var users = getUsers(req.params.searchTerm);
+        var communities = getCommunities(req.params.searchTerm);
+
+        var userResults = [];
+        if (users.length > 0) {
+            for (let i = 0; i < users.length; i++) {
+                userResults.push(users[i]);
+            }
+        }
+
+        var commResults = [];
+        if (communities.length > 0) {
+            for (let i = 0; i < communities.length; i++) {
+                commResults.push(communities[i]);
+            }
+        }
+
+        var esResults = {};
+        if (searchResults.length > 0) {
+            for (let i = 0; i < searchResults.length; i++) {
+                if (esResults.hasOwnProperty(searchResults[i]._index)) {
+                    esResults[searchResults[i]._index].push(searchResults[i]._source);
+                }
+                else {
+                    esResults[searchResults[i]._index] = [];
+                    esResults[searchResults[i]._index].push(searchResults[i]._source);
+                }
+            }
+        }
+
+        results["users"] = userResults;
+        results["communities"] = commResults;
+        results["videos"] = esResults;
+
+    } catch (err) {
+        return res.send(500).send({"message": "Error searching"});
+    }   
+
+    return res.send(200).send({"results": results});
+};
+
+app.get('/search/:searchTerm', search);
